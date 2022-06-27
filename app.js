ee414f339f68fd7a4e30325893afcedc26cdecfe
NicoDNico <Nicolasdnicora@gmail.com>
2022-06-27 14:51:42 -0300

    some changes
    okey so having the python folder with the web scraper lead to some problems (dont ask me) so i removed it and its now in another folder.
    Added a toggle between CsvFile and letterboxd id mode.
    The toggle lead to some changes.

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var ratin = [];
 var namesother = [];
 var ratinother = [];
 var CsvDataParsed = 'parse 1 empty';
-var CsvDataOther = 'parsed 2 empty';
+var CsvDataOtherParsed = 'parsed 2 empty';
+var MemberDataParsed = '';
+var MemberDataOtherParsed = '';
 
 var dropfile = [];
 var dropfile2 = [];
@@ -29,12 +31,18 @@ const topbar = document.getElementById("topbar");
 
 const test = document.getElementById("test");
 
+const userfile = document.getElementById("userfile");
+
+const comparefile = document.getElementById("comparefile");
+
+var moviesshared = [];
+var moviesnotshared = [];
 
 
-
-async function  DataofMovies(){
-  let datalenght = Object.keys(CsvDataParsed).length;
-  CsvDataParsed.sort(function(a, b){
+async function  DataofMovies(CVSdata){
+  let datalenght = Object.keys(CVSdata).length;
+  if(check == false){
+  CVSdata.sort(function(a, b){
     if (a.Rating<b.Rating){
       return 1;
     }
@@ -42,18 +50,25 @@ async function  DataofMovies(){
       return -1;
     }
     return 0;
-  });
-  console.log(CsvDataParsed);
+  })};
     for(let i = 0; i < datalenght -1 ; i++){
-      let objdata = Object.entries(CsvDataParsed[i]);
+      let objdata = Object.entries(CVSdata[i]);
+      if(check == false){
         if(objdata[4][1]>=4){
         names.push(objdata[1][1]);
         ratin.push(objdata[4][1]);
         links.push(objdata[3][1]);
         }
         else{
+        }}
+      else{
+          if(objdata[4][1]>=7){
+          names.push(objdata[1][1]);
+          ratin.push(objdata[4][1]);
         }
-    }
+        else{
+        }
+    }}
     console.log('test DataofMovies');
   return [names, ratin]
 }
@@ -66,11 +81,13 @@ async function  DataofMovies2(csvdata){
     }
     console.log('test DataofMovies2');
   return [namesother, ratinother]
-}
+};
 
 
 // saves the data of primary csv
 btn.addEventListener("pointerdown", async function (e) {
+  topbar.className = 'barraleft shadowred ' ;
+  if(check == false){
     e.preventDefault
     let input = csvFile.files[0];
     if(check1 == true){
@@ -86,16 +103,32 @@ btn.addEventListener("pointerdown", async function (e) {
       console.log(names.length);
       await csvreader2();
       await adddiv(); 
-      await comparator();     
+      await comparator();
        
     };
     reader.readAsText(input);
+  }
+  else{
+    let member = textbox.value; 
+    let member2 = textboxOther.value;
+    console.log("You're:"+member);
+    console.log("Other is:"+member2);
+    let MemberData = await letterboxdAPicall(member);
+    MemberDataParsed = Papa.parse(MemberData,papaconfig).data;
+    let MemberDataOther = await letterboxdAPicall(member2);
+    MemberDataOtherParsed = Papa.parse(MemberDataOther,papaconfig).data;
+    await DataofMovies(MemberDataParsed);
+    await DataofMovies2(MemberDataOtherParsed);
+    await adddiv();
+    await comparator();
+  }
+  topbar.className = 'barraleft shadowgreen ' ;
 });
 
 
 
 async function csvreader2(){
-  let input = csvFile.files[0];
+  let input = csvFile2.files[0];
   if(check2 == true){
     input = dropfile2;
   }
@@ -121,7 +154,6 @@ async function adddiv(){
   const imgurl = document.createElement('img');
   const divtinfo = document.createElement('h3');
   divtinfo.className = 'info';
-  topbar.className = 'barraleft shadowred ' ;
   for(let i = 0; i < names.length ; i++){
     let divtinfo_cloned = divtinfo.cloneNode(true);
     divtinfo_cloned.id = 'info' + i;
@@ -162,7 +194,6 @@ async function adddiv(){
   for(let i =0; i <names.length;i++){
 
   }
-  topbar.className = 'barraleft shadowgreen ' ;
 }
 function removedivs(){
  btn.style.display = 'none';
@@ -176,7 +207,6 @@ function removedivs(){
 async function tmdbapi(x){
   let title = x.replace(/\s+/g,'%20');
   let moviedata = await fetch("https://api.themoviedb.org/3/search/multi?"+apikey+"&language=en-US&query="+title).then(response => response.json());
-  console.log('test');
   if(moviedata.results[0] == undefined){
     let nose = {
       poster_path: '/fj21HwUprqjjwTdkKC1XZurRSpV.jpg'
@@ -198,8 +228,7 @@ function comparator(){
   for(let i=0; i < namesother.length; i++){
     if(names.indexOf(namesother[i]) > -1){
       let namesposition = names.indexOf(namesother[i]);
-      console.log("Este nombre esta en el array original: "+namesother[i]);
-      console.log('En la posicion: '+names.indexOf(namesother[i]));
+      moviesshared.push(namesother[i]);
 
       let two_cloned = two.cloneNode(true);
       two_cloned.id = 'ratingtwo'+namesposition;
@@ -211,24 +240,35 @@ function comparator(){
       
   }
     else{
-      console.log('no comparten esta pelicula ');
-      console.log(namesother[i]);
+      moviesnotshared.push(namesother[i]);
     }
   }
+  console.log(moviesshared);
+  console.log(moviesnotshared);
 };
-
 
 // page maker
 
-test.addEventListener("pointerdown", pagemaker);
-let blob = "<body>Hola</body>"
-function pagemaker(){
-  console.log("test")
-   let a = new File(blob,testpage.txt,{
-    type: "Text/plain",
-  });
-  window.open(a);
-  
+const textbox = document.getElementById('search');
+const textboxOther = document.getElementById('searchOther');
+
+test.addEventListener("pointerdown", async function(){
+  let member = textbox.value; 
+  let member2 = textboxOther.value;
+  console.log("You're: "+member);
+  console.log("Other is: "+member2);
+  console.log("algo raro esta pasando")
+  let MemberData = await letterboxdAPicall(member);
+  MemberDataParsed = Papa.parse(MemberData,papaconfig).data;
+  console.log(MemberDataParsed);
+  // let MemberDataOther = await letterboxdAPicall(member2);
+  // MemberDataOtherParsed = Papa.parse(MemberDataOther,papaconfig).data;
+  // console.log(MemberDataOtherParsed);
+});
+
+async function letterboxdAPicall(memberAPI){
+  let memberjson = await fetch('http://localhost:8000/api/'+ memberAPI).then(response => response.json());
+  return memberjson;
 };
 
 // drop box. 
@@ -266,4 +306,33 @@ document.addEventListener("dragover", event=>{
 });
 document.addEventListener("drop", event=>{
   event.preventDefault();
+});
+
+const checkbox = document.getElementById("switch");
+const TextBoxOtherContainer = document.getElementById("searchBoxOtherContainer");
+const textboxContainer = document.getElementById("searchBoxContainer");
+var check = false;
+checkbox.addEventListener("change", function(){
+  console.log('x');
+  if(checkbox.checked){
+    console.log('checked');
+    dropother.style.display = 'none';
+    dropyourself.style.display = 'none';
+    userfile.style.display = 'none';
+    comparefile.style.display = 'none';
+    textboxContainer.style.display = 'block';
+    TextBoxOtherContainer .style.display = 'block';
+    check = true;
+  }
+  else{
+    console.log('unchecked');
+    dropother.style.display = 'block';
+    dropyourself.style.display = 'block';
+    userfile.style.display = 'block';
+    comparefile.style.display = 'block';
+    textboxContainer.style.display = 'none';
+    TextBoxOtherContainer .style.display = 'none';
+    check = false;
+  }
+  
 });
